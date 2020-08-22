b3a6a939b2907e6483f88b0737fde2b84b18ac7a
superColin <supercolin.dev@gmail.com>
2020-08-22 13:07:26 -0400

    command and args separated, some white space edge cases taken care of

--- python/discordBot.py
+++ python/discordBot.py
@@ -1,5 +1,6 @@
 # Big Ups to:
 # https://realpython.com/how-to-make-a-discord-bot-python/
+import re # regex for later
 
 # ---
 # Use .env file for configuration settings
@@ -15,7 +16,12 @@ PREFIX = os.getenv('DISCORD_PREFIX')
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
+# ---
+# Link DB
 
+from tinydb import TinyDB, Query
+
+db = TinyDB('discordDB.json')
 
 # ---
 # Set up bot
@@ -49,34 +55,76 @@ async def on_ready():
     # print(f'Guild Members:\n - {members}')
 
 
-# @client.event
-# async def on_some(member):
-#     await member.create_dm()
-#     await member.dm_channel.send(
-#         'pssst.. hey kid, you should throw SuperColin a dollar'
-#     )
-
 
 @client.event
 async def on_message(message):
-    if message.author.bot:
+    if message.author.bot: # ignore bot messages
         return
 
-    if message.content == '99!':
-        response = '99!'
-        # print(message.author.id)
-        await message.channel.send(response)
 
+    command = ''
+    args = ''
+
+    if message.content.startswith(PREFIX) == False: 
+        # Ignore non prefixed message
+        return
+    else:
+        # Format message, seperate command and args
+            # Remove extra spaces
+        slicedMessage = re.sub(' +', ' ', message.content[(len(PREFIX)):]) 
+        
+        
+        if slicedMessage.find(' ') == 0:
+                # Remove a space at the begining
+            slicedMessage = slicedMessage[1:]
+
+        
+        endOfCommand = slicedMessage.find(' ')
+        if slicedMessage.find(' ') == 0:
+            command = slicedMessage[0:]
+        elif slicedMessage.find(' ') == -1:
+            command = slicedMessage[0:]
+            args = False
+        else:
+            command = slicedMessage[0:endOfCommand]
+            args = slicedMessage[endOfCommand:]
+
+    await message.channel.send(command)
+    await message.channel.send(args)
+
+
+
+
+
+
+    if command == 'test':
+        response = '99'
+        await message.channel.send(response)
+    
+
+    # ---
     # My special commands :P
     if message.author.id != 663600377677086730: # the number is my discord id
         return
-        # Actual cmds:
     else:
-        if message.content == '?':
+        # Actual cmds:
+
+        if command == 'testMe':
             response = '99!?'
             await message.channel.send(response)
+
+
         if message.content == "write db":
-            
+            print('writing to db')
+            db.insert({"type":"test", "data": "a string"})
+            print('db write complete')
+
+
+        if message.content == 'list db':
+           await message.channel.send(db.all())
+
+
+
 
 
 
