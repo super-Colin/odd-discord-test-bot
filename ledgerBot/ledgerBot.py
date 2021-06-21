a7fffb9074d29c5b74d00d4a355bac7d7e698ee2
superColin <supercolin.dev@gmail.com>
2021-06-21 17:19:41 -0400

    added emoji functionality, added functionality for reaction based responses

--- ledgerBot/ledgerBot.py
+++ ledgerBot/ledgerBot.py
@@ -1,17 +1,25 @@
-# This example requires the 'members' privileged intents
-
-
+# pip install python-dotenv, discord, bs4, requests...
+# Getting and working with Google spreadsheet
 from datetime import datetime, date
 from google.oauth2 import service_account
 import pygsheets
+
+# Scraping a price for Etherium
+from bs4 import BeautifulSoup
+import requests
+
+# Discord bot library and command extension
+import discord
+from discord.ext import commands
+import random
+import asyncio
+
+
 # ---
 # Use .env file for configuration settings
-
 import os
-    # Load os module to access other files (.env)
 from dotenv import load_dotenv
-    # Import load_dotenv command from python-dotenv module
-    # Use the CLI cmd "pip install -U python-dotenv" to make sure you have dotenv installed
+# Use the CLI cmd "pip install -U python-dotenv" to make sure you have dotenv installed
 load_dotenv() # Get variables from .env file
 PREFIX = os.getenv('DISCORD_PREFIX')
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -20,22 +28,32 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 SPREADSHEET_ID = os.getenv('GSHEETS_SPREADSHEET_ID')
 RANGE_NAME = os.getenv('GSHEETS_RANGE_NAME')
 GSHEETS_SPREADSHEET_URL_KEY = os.getenv('GSHEETS_SPREADSHEET_URL_KEY')
-    # ---
+# ---
+
 serviceAccountFile = './gCredentials.env.json'
 
-import discord
-from discord.ext import commands
-import random
+ledgerColumns = [
+    'Date',
+    'Time',
+    'Action',
+    'From Account',
+    'From Account ID',
+    'From Account Balance',
+    'To Account',
+    'To Account ID',
+    'To Account Balance',
+    'Amount',
+    'Description'
+]
 
-description = '''An example bot to showcase the discord.ext.commands extension
-module.
-There are a number of utility commands being showcased here.'''
 
+
+
+
+botDescription = '''The ODD oracle, always expan.'''
 intents = discord.Intents.default()
 # intents.members = True
-
-# bot = commands.Bot(command_prefix=PREFIX, description=description, intents=intents)
-bot = commands.Bot(command_prefix=PREFIX, description=description)
+bot = commands.Bot(command_prefix=PREFIX, description=botDescription)
 
 @bot.event
 async def on_ready():
@@ -51,15 +69,25 @@ async def add(ctx, left: int, right: int):
 
 
 
-# @bot.command()
-# async def pay(ctx, toMember: discord.Member, amount):
-#     fromMember = ctx.author
-#     """Send tokens to someone"""
-#     await ctx.send(f'working on it... sending {amount} tokens to {toMember} from {fromMember}')
+@bot.command()
+async def pay(ctx, toMember: discord.Member, amount):
+    fromMember = ctx.author
+    """Send tokens to someone"""
+    await ctx.send(f'Send {amount} tokens to {toMember}? \nUse a 👍 reaction, to confirm')
+
+    def check(reaction, user):
+        return user == ctx.author and str(reaction.emoji) == '👍'
+
+    try:
+        reaction, user = await bot.wait_for('reaction_add', timeout=20.0, check=check)
+    except asyncio.TimeoutError:
+        await ctx.send('👎 Ahh for real')
+    else:
+        await ctx.send('👍 Thanks brah')
 
 
 
-
+# ~~~~~~~~~~~~~ Ledger Functions ~~~~~~~~~~~~~
 @bot.command()
 async def headers(ctx):
     wks = await getWorkSheet()
@@ -76,12 +104,13 @@ async def headers(ctx):
 
 
 @bot.command()
-async def balance(ctx):
-# async def balance(ctx, targetAccount: discord.Member):
-    msg = ''
-    targetAccountId = str(ctx.author.id)
-    # targetAccountId = targetAccount.id
-    balance = ''
+# async def balance(ctx):
+async def balance(ctx, targetAccount: discord.Member = None):
+    if targetAccount == None:
+        targetAccountId = targetAccountId = str(ctx.author.id)
+    else:
+        targetAccountId = str(targetAccount.id)
+    
     wks = await getWorkSheet()
 
     # create a list containing all rows from the sheet
@@ -96,21 +125,25 @@ async def balance(ctx):
 
     
     #loop through table
-    foundIt = 'No'
-    action = ''
-    date = ''
-    fromAccount = ''
+    foundIt, action, date, fromAccount, toAccount, balance, description = False,'','','','','',''
     for row in table:
         #check if it was to our target account
         if row[ledgerIndexes['To Account ID']] == targetAccountId :
-            foundIt = 'YES!!'
+            foundIt = True
             balance = row[ledgerIndexes['To Account Balance']]
             action = row[ledgerIndexes['Action']]
             date = row[ledgerIndexes['Date']]
             fromAccount = row[ledgerIndexes['From Account']]
+            toAccount = row[ledgerIndexes['To Account']]
+            description = row[ledgerIndexes['Description']]
 
-    msg = (f'Found account: {foundIt} \n The balance of {targetAccountId} is {balance} \n from a {action} on {date} from {fromAccount}')
-    # msg = (f'{theRows}')
+    msg = (f"""
+The balance of {toAccount} is <:oddcoin:855612234558341161>{balance}<:oddcoin:855612234558341161>
+From a {action} on {date} from {fromAccount}
+For: {description}
+    """)
+    if not foundIt:
+        msg = 'Did not find an account with that @ID'
     await ctx.send(msg)
 
 
@@ -142,6 +175,26 @@ async def sheetDump(ctx):
     await ctx.send(msg)
 
 
+
+
+# ~~~~~~~~~~~~~ ETH Functions ~~~~~~~~~~~~~
+@bot.command()
+async def eth(ctx):
+    url = 'https://ethereumprice.org/'
+    # html = urllib.request.urlopen(url)
+    req = requests.get(url)
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+    ethPriceCurrency = soup.find('div', {'id':'coin-price'}).find('span',{'class':'currency-symbol'})
+    ethPrice = soup.find('div', {'id':'coin-price'}).find('span',{'class':'value'})
+
+    msg = (f'Eth is currently at {ethPriceCurrency.text}{ethPrice.text}')
+    await ctx.send(msg)
+
+
+
+
+# ~~~~~~~~~~~~~ Utility Functions ~~~~~~~~~~~~~
 async def getWorkSheet():
     client = pygsheets.authorize(service_file=serviceAccountFile)
     sheet = client.open_by_key(GSHEETS_SPREADSHEET_URL_KEY)
@@ -150,18 +203,7 @@ async def getWorkSheet():
 
 def getLabelIndexes(tableHeaders):
     # figure out what columns are the ones we want
-    ledgerIndexes = {
-        'Date': '',
-        'Time': '',
-        'Action': '',
-        'From Account': 0,
-        'From Account ID': 0,
-        'From Account Balance': 0,
-        'To Account': 0,
-        'To Account ID': 0,
-        'To Account Balance': 0,
-        'Amount': 0,
-    }
+    ledgerIndexes = {x:0 for x in ledgerColumns}
     for columnIndex, columnHeader in enumerate(tableHeaders):
         for labelString in ledgerIndexes:
             if columnHeader == labelString:
@@ -180,6 +222,20 @@ def getLabelIndexes(tableHeaders):
 
 
 
+# ~~~~~~~~~~~~~ Example Command Functions ~~~~~~~~~~~~~
+
+@bot.command()
+async def thumb(ctx):
+    await ctx.send('Send me that 👍 reaction, mate')
+    def check(reaction, user):
+        return user == ctx.author and str(reaction.emoji) == '👍'
+
+    try:
+        reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
+    except asyncio.TimeoutError:
+        await ctx.send('👎 Ahh for real')
+    else:
+        await ctx.send('👍 Thanks brah')
 
 
 @bot.command()
